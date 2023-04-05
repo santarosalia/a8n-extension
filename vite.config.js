30dc3d656d764aaacf81487fd4cdc9ea9d6693d8
dope <mdc@kakao.com>
2023-04-05 11:20:37 +0900

    vite config edit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { crx } from '@crxjs/vite-plugin'
 import manifest from './manifest.json' assert { type: 'json' }
 // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
 import vuetify from 'vite-plugin-vuetify'
-import path from 'path'
+import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -16,8 +16,15 @@ export default defineConfig({
     alias : [
       {
         find : '@',
-        replacement : path.resolve(__dirname, 'src')
+        replacement : resolve(__dirname, 'src')
       }
     ]
+  },
+  build : {
+    rollupOptions : {
+      input : {
+        main : resolve(__dirname, 'index.html')
+      }
+    }
   }
 })
