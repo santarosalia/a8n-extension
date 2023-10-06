111b0079bb7c26903c0e164efe5314f59a279716
dope <dhkim@edentns.com>
2023-10-06 20:35:21 +0900

    delete vue

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 import App from './App'
@@ -16,7 +16,7 @@ if (!root) {
 }
 
 ReactDOM.createRoot(root).render(
-    <React.StrictMode>
+    <StrictMode>
         <App />
-    </React.StrictMode>
+    </StrictMode>
 );
