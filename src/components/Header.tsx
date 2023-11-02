264c502a56511e0c45250070a343443f3ad41602
gqob <dhkim@edentns.com>
2023-11-02 18:11:12 +0900

    gohome add

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,14 @@
+import { openWindow } from "@/ts/api/CrxApi";
 import { AppBar, Box } from "@mui/material"
 
 export default () => {
     const imageURL = chrome.runtime.getURL('/rgb.gif');
-    
+    const goHome = () => {
+        openWindow(import.meta.env.VITE_HOME);
+    }
     return (
         <AppBar sx={{backgroundColor : 'black', height : '30px'}}>
-            <Box margin={'auto'}>
+            <Box onClick={goHome} margin={'auto'}>
                 <img src={imageURL} width={'30px'} height={'30px'}/>
             </Box>
         </AppBar>
